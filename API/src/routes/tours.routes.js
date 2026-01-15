16461cd1b3ba7789180a27bf9bad0b86563c9d97
Luis Benavides <lfbenav@gmail.com>
2026-01-14 22:34:10 -0600

    Cambuois

--- API/src/routes/tours.routes.js
+++ API/src/routes/tours.routes.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const controller = require("../controllers/tours.controller");
 
+const auth = require("../middlewares/auth.middleware");
+const { requirePermission } = require("../middlewares/permission.middleware");
+
 router.get("/test", controller.test);
 
 router.get("/getAll", controller.getAll);
+// router.get("/getAll", auth, requirePermission("MANAGE_TOURS"), controller.getAll); //! Asi es como se pone para que solo el que tenga x permiso pueda acceder
 router.get("/getAllActive", controller.getAllActive);
 
 module.exports = router;
